225821f7131f6537e7b5ad528756bbfaec3a66cf
Alexandre Aubin <alexandre.aubin@cern.ch>
2016-12-13 21:42:13 +0000

    Misc fixes

--- yunohost-deploy-jessie-amd64/00_init.sh
+++ yunohost-deploy-jessie-amd64/00_init.sh
@@ -9,8 +9,3 @@ done
 apt-get update                       2> /root/000_update.err         | tee /root/000_update.log          
 apt-get install -y tasksel sudo git  2> /root/010_installtasksel.err | tee /root/010_installtasksel.log  
 tasksel install standard ssh-server  2> /root/020_installbase.err    | tee /root/020_installbase.log  
-#apt-get install -y `tasksel --task-packages standard | grep -v rpcbind | grep -v nfs-common`       2> /root/40_installbase.err    | tee /root/40_installbase.log     
-#if [ $? != 0 ]; then
-#    apt-get update --fix-missing
-#    apt-get install -y `tasksel --task-packages standard | grep -v rpcbind | grep -v nfs-common`   2> /root/40_installbase.err    | tee /root/40_installbase.log     
-#fi
--- yunohost-deploy-jessie-amd64/analyze.sh
+++ yunohost-deploy-jessie-amd64/analyze.sh
@@ -4,10 +4,11 @@ if [ -z $LOG_DIR ]; then
     LOG_DIR=$1
 fi
 
-PULLED_DIR= `ls -d $LOG_DIR/*/`
+PULLED_DIR=`ls -d $LOG_DIR/*/`
 if [ -z $PULLED_DIR ]; then
     echo "No directory found for pulled files (?)" | tee $LOG_DIR/report.log
     exit
 fi
 
-grep -r -C2 -i -E "(warning|no such|[^b]error|closed connection|unknown|traceback|unable)" $PULLED_DIR/*.{log,err} > $LOG_DIR/report.log
+cd $PULLED_DIR
+grep -r -C2 -i -E "(warning|no such|[^b]error|closed connection|unknown|traceback|unable)" ./*.{log,err} > $LOG_DIR/report.log
